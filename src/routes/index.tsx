67c86f4d150292f19afa51a74fd8be52366fa5cb
alisteam0 <alisteam0001@gmail.com>
2026-06-25 18:55:23 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -143,7 +143,7 @@ function LandingPage() {
           style={{
             display: "flex",
             width: "max-content",
-            animation: "marquee 10s linear infinite",
+            animation: "marquee 15s linear infinite",
           }}
         >
           {Array.from({ length: 12 }, (_, i) => (
